4e296d4305cfd26a40d08b7c08a10f94be5c73bb
NikitaKalashnikov2006 <kalaha2006@gmail.com>
2025-07-09 09:02:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 // Инициализация Telegram WebApp
 const tg = window.Telegram.WebApp;
+ tg.ready();
+
+
+  
+ const availableScreenWidth = window.screen.availWidth;
+ const availableScreenHeight = window.screen.availHeight;
+ if (availableScreenWidth < 1440 && availableScreenHeight < 3220){
+tg.requestFullscreen();
+ }
 let tonConnectUI = null;
 
 // Инициализация при загрузке
